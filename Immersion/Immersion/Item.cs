40acd61cff206ee26fdf08c2253ab444d19895ea
Taylor <ttaylor11@elon.edu>
2012-11-18 22:11:40 -0500

    Added item class
    working on collection system

---
+++ Immersion/Immersion/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using System.Collections;
+namespace Immersion
+{
+    class Item : Sprite
+    {
+        private bool isCollected;
+        protected float myPositionZ;
+        protected float myVelocityZ;
+        protected Texture2D shadowImage;
+        protected PlatformSprite currentPlatform;
+
+        public Item(Texture2D image, Texture2D shadow, Vector2 position)
+            : base(image, position)
+        {
+            this.shadowImage = shadow;
+        }
+
+        public void Bobble()
+        {
+
+        }
+    }
+}
